8d7cf9fd1c5518b1b3f01cc108c85ff87b23da2a
Tok736 <to_ev@mail.ru>
2019-11-29 18:27:32 +0300

    added uml

--- cmake-build-debug/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kotik/cpp_project/task-manager/project/googletest
-# Build directory: /home/kotik/cpp_project/task-manager/project/cmake-build-debug/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kotik/cpp_project/task-manager/project/googletest/googlemock
-# Build directory: /home/kotik/cpp_project/task-manager/project/cmake-build-debug/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
