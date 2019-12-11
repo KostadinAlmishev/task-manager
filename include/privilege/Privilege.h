dfd8264baa7380210622412a1ad9e54723d41f95
danissomo <dan.2k64@gmail.com>
2019-12-11 10:26:33 +0300

    добавил класс привелегий, дописал смену пароля, пара тестов

---
+++ include/privilege/Privilege.h
@@ -0,0 +1,8 @@
+//
+// Created by daniil on 11.12.2019.
+//
+
+#ifndef TASK_MANAGER_PRIVILEGE_H
+#define TASK_MANAGER_PRIVILEGE_H
+
+#endif //TASK_MANAGER_PRIVILEGE_H
---
+++ test/services/userSericeTest.cpp
@@ -0,0 +1,5 @@
+//
+// Created by daniil on 07.12.2019.
+//
+
+#include "userSericeTest.h"
